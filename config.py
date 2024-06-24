4369f6b93942a2abaf4ab9bdfb07cac7393bcd08
ssunniezz <ssunniez@gmail.com>
2024-06-24 19:29:51 +0700

    update sql path

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
-
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
 class ProductionConfig(Config):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:sunsundb26@database-1.cbmeu2kq8fk8.us-east-1.rds.amazonaws.com:5432/employee')
